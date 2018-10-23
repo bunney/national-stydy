4b8a7af2231908f9a2df6d605c31fe34085d7374
dql <qiuliang.du@duobei.com>
2018-10-23 21:52:35 +0800

    更新

--- demo/koa-plugin/use-useragent.js
+++ demo/koa-plugin/use-useragent.js
@@ -1,12 +1,16 @@
 const Koa = require("koa")
 const userAgent = require("koa-useragent")
+const log = require("./log-plugin")
 
 const app = new Koa()
 
-app.use(userAgent)
+const logConfig = {format: (text) => {return `===== ${text} =====`}}
 
-app.use(async (ctx, next) => {
-    console.log(require("util").inspect(ctx.userAgent))
-})
+app.use(userAgent)
+app.use(log(logConfig))
+
+// app.use(async (ctx, next) => {
+//     // console.log(require("util").inspect(ctx.userAgent))
+// })
 
 app.listen(3000)
